d1f40d1d9070074548fb8646feec955d5a6edd22
20236051@stud.fci-cu.edu.eg <sherifabdulmaged@gmail.com>
2025-04-09 17:50:06 +0200

    initial

---
+++ src/main/java/com/AniMy/demo/AniMyApplication.java
@@ -0,0 +1,13 @@
+package com.AniMy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniMyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AniMyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AniMy/demo/AniMyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AniMy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniMyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
